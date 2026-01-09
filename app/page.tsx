a75172d2232e698da1a8c8a15dd5450a7e5646b2
AJAYK-1 <ajaykumartp10@gmail.com>
2026-01-09 20:54:39 +0530

    "Navbar"

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ExploreBtn from '@/components/ExploreBtn'
 import React from 'react'
 
 const Home = () => {
@@ -5,6 +6,15 @@ const Home = () => {
     <section>
       <h1 className='text-center'> This is the Home page </h1>
       <p> Welcome to the Home page. </p>
+      <ExploreBtn />
+      <div className='mt-20 space-y-7'>
+        <h3>Featured Events</h3>
+        <ul className='events'>
+          {[1, 2, 3, 4, 5].map((event) => (
+            <li key={event}>Event {event}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
