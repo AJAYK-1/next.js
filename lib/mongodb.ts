835f4b73d553a481d60f8714eafebe2b7033132e
AJAYK-1 <ajaykumartp10@gmail.com>
2026-01-15 15:03:18 +0530

    "mongodb"

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -5,5 +5,19 @@ type MongooseCache = {
     promise: Promise<typeof mongoose> | null;
 }
 declare global {
-    let mongoose: MongooseCache | undefined;
+    var mongoose: MongooseCache | undefined;
+}
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+if (!MONGODB_URI) {
+    throw new Error(
+        `Please define the MONGODB_URI environment variable inside .env`
+    )
+}
+
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null }
+
+if (!global.mongoose) {
+    global.mongoose = cached;
 }
