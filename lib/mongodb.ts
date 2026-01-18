cf3cb0f88f7d1df115236ef8759dac330c9353fd
AJAYK-1 <ajaykumartp10@gmail.com>
2026-01-18 21:49:31 +0530

    "event model"

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -21,3 +21,35 @@ let cached: MongooseCache = global.mongoose || { conn: null, promise: null }
 if (!global.mongoose) {
     global.mongoose = cached;
 }
+
+async function connectDB(): Promise<typeof mongoose> {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        if (!MONGODB_URI) {
+            throw new Error(
+                'Please define the MONGODB_URI environment variable inside .env.local'
+            );
+        }
+        const options = {
+            bufferCommands: false,
+        };
+
+        cached.promise = mongoose.connect(MONGODB_URI!, options).then((mongoose) => {
+            return mongoose;
+        });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (error) {
+        cached.promise = null;
+        throw error;
+    }
+
+    return cached.conn;
+}
+
+export default connectDB;
