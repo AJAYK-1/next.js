d80c24e1d72e61a47c6dad15e631002244ea778d
AJAYK-1 <ajaykumartp10@gmail.com>
2026-01-21 21:31:12 +0530

    "cloudinary setup"

--- app/api/events/routes.ts
+++ app/api/events/routes.ts
@@ -1,17 +1,26 @@
 import Event from "@/database/event.model";
 import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from 'cloudinary'
 
 export async function POST(req: NextRequest) {
     try {
         await connectDB()
         const formData = await req.formData()
         let event;
+
         try {
             event = Object.entries(formData.entries())
         } catch (error) {
             return NextResponse.json({ message: "Invalid JSON data format" }, { status: 400 })
         }
+
+        const file = formData.get('image') as File;
+
+        if (!file) return NextResponse.json({ message: 'Image file is required' }, { status: 400 })
+
+        const arrayBuffer = await file.arrayBuffer()
+        const buffer = Buffer.from(arrayBuffer)
 
         const createdEvent = await Event.create(event)
         return NextResponse.json({ message: "Event created successfully", event: createdEvent }, { status: 201 })
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com'
+      }
+    ]
+  },
+  skipTrailingSlashRedirect: true,
   reactCompiler: true,
   cacheComponents: true,
   experimental: {
