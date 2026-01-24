415e00120a3d7bbecdd7c5f1deb89453d24ae848
AJAYK-1 <ajaykumartp10@gmail.com>
2026-01-24 21:55:21 +0530

    "data fetch|"

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import EventCard from '@/components/EventCard'
 import ExploreBtn from '@/components/ExploreBtn'
-import { events } from '@/lib/constants'
+import { IEvent } from '@/database/event.model'
 
-const Home = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_HOST_URL
+
+const Home = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`)
+  const { events } = await response.json()
+
   return (
     <section>
       <h1 className='text-center'> This is the Home page </h1>
@@ -11,7 +16,7 @@ const Home = () => {
       <div className='mt-20 space-y-7'>
         <h3>Featured Events</h3>
         <ul className='events'>
-          {events.map((event) => (
+          {events.map((event: IEvent) => (
             <li key={event.title}>
               <EventCard {...event} />
             </li>
